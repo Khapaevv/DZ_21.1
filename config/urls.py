3b9e9324fc40487e5682f1bdca79f6b63c314e2b
Vladimir Khapaev <khapaev@mail.ru>
2024-07-20 21:53:50 +0300

    commit all files

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from catalog import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('contacts/', views.contacts, name='contacts'),
+    path('', include('catalog.urls', namespace='catalog'))
+]
