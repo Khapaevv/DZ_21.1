5c1bbbf422b05f8fac86aae7a90cfad46aac166b
Vladimir Khapaev <khapaev@mail.ru>
2024-07-26 14:51:31 +0300

    create models (Product and Category)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        # ordering = ['', '', '']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Наименование продукта",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        **NULLABLE
+    )
+    image = models.ImageField(
+        upload_to="images/",
+        verbose_name="Изображение",
+        help_text="Загрузите фото продукта",
+        **NULLABLE,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+    )
+    price = models.IntegerField(
+        verbose_name="Цена за покупку", help_text="Введите цену за покупку продукта"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        # ordering = ['', '', '']
+
+    def __str__(self):
+        return self.name
