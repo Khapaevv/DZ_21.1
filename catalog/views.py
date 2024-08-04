771e96afe54c2576d49142cb7da0c4577e2034a3
Vladimir Khapaev <khapaev@mail.ru>
2024-08-04 23:04:19 +0300

    end 3 task

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from catalog.models import Product
 
 
-def Product_list(request):
+def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, 'products_list.html', context)
 
-# def home(request):
-#     return render(request, 'home.html')
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'products_detail.html', context)
+
+def base(request):
+    return render(request, 'base.html')
 
 def contacts(request):
     if request.method == 'POST':
@@ -18,3 +24,4 @@ def contacts(request):
 
         print(f'{name} написал следующее сообщение: {message}, контактный телефон: {phone}')
     return render(request, 'contacts.html')
+
--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from catalog.views import Product_list
+from catalog.views import products_list
+
 
 urlpatterns = [
-    # path('', Product_list),
     path('admin/', admin.site.urls),
-    # path('contacts/', views.contacts, name='contacts'),
     path('', include('catalog.urls', namespace='catalog')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
