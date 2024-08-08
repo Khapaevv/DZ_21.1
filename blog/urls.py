e07f8160828df38ea638c407ed73a1ee3de51e4f
Vladimir Khapaev <khapaev@mail.ru>
2024-08-08 17:08:18 +0300

    error slug

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from blog.apps import BlogConfig
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView, blog_is_publication
+from catalog.views import ProductListView, ContactsPageView, PriductDetailView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path("create/", BlogCreateView.as_view(), name="create"),
+    path("", BlogListView.as_view(), name="list"),
+    path("view/<int:pk>/", BlogDetailView.as_view(), name="view"),
+    path("update/<int:pk>/", BlogUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", BlogDeleteView.as_view(), name="delete"),
+    path("activity/<int:pk>/", blog_is_publication, name="blog_is_publication")
+]
--- catalog/views.py
+++ catalog/views.py
@@ -7,14 +7,30 @@ from catalog.models import Product
 class ProductListView(ListView):
     model = Product
 
+# def products_list(request):
+#     products = Product.objects.all()
+#     context = {"products": products}
+#     return render(request, "products_list.html", context)
+
+
 class PriductDetailView(DetailView):
     model = Product
 
-def base(request):
-    return render(request, "base.html")
+# def products_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {"product": product}
+#     return render(request, "products_detail.html", context)
+
+
+class BasePageView(TemplateView):
+    template_name = "catalog/base.html"
+
+# def base(request):
+#     return render(request, "base.html")
 
 class ContactsPageView(TemplateView):
     template_name = "catalog/contacts.html"
+
 
     def post(self, request, *args, **kwargs):
         if request.method == "POST":
@@ -26,4 +42,15 @@ class ContactsPageView(TemplateView):
                 f"{name} написал следующее сообщение: {message}, контактный телефон: {phone}"
             )
         return render(request, "catalog/contacts.html")
+
+# def contacts(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         phone = request.POST.get("phone")
+#         message = request.POST.get("message")
+#
+#         print(
+#             f"{name} написал следующее сообщение: {message}, контактный телефон: {phone}"
+#         )
+#     return render(request, "contacts.html")
 
