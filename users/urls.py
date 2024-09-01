805cde1c639f9bdaeeeb7e35a02db42d7e3b6c8f
Vladimir Khapaev <khapaev@mail.ru>
2024-09-02 00:12:23 +0300

    reset password working, but )

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from users.apps import UsersConfig
 from django.urls import path
 
-from users.views import UserCreateView, email_verification, CustomLoginView
+from users.views import UserCreateView, email_verification, CustomLoginView, UserPasswordResetView, NoMailView
 
 app_name = UsersConfig.name
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', UserCreateView.as_view(), name='registration'),
     path('email-confirm/<str:token>', email_verification, name='email-confirm'),
-    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/', UserPasswordResetView.as_view(), name='password_reset'),
+    path('no_mail/', NoMailView.as_view(), name='no_mail.html')
 ]
